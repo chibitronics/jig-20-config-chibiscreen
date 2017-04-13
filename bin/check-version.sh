382b191da03e1ac4767f6f4c0ebd2629f166604a
bunnie <bunnie@kosagi.com>
2017-04-13 00:57:52 -0400

    more fixup
    Signed-off-by: bunnie <bunnie@kosagi.com>

---
+++ bin/check-version.sh
@@ -0,0 +1,20 @@
+#!/bin/sh -e
+max=3
+
+source ./00-test-lib.sh
+
+for i in $(seq 1 ${max})
+do
+    reset_board
+    
+    if timeout 3 grep -q "Chibiscreen build 873000deb3bb06d64056db415f51ee53a24b6b76" ${uart}
+    then
+	echo q > ${uart}
+	echo "Version OK"
+	exit 0
+    fi
+    echo "Timed out, trying again..."
+done
+
+echo "Failed to get correct version."
+exit 1
